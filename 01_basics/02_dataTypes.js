d551fa962a78f892fd4c24ae68102b6fffc7e3a8
Abrar12cs <135686768+Abrar12cs@users.noreply.github.com>
2023-10-27 18:17:25 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,29 @@
+"use strict"; // treat all JS code as newer version standards 
+
+// alert( 3 + 3) // we are using nodejs, not browser alert supported in browser
+
+console.log(3 
+    +
+     3) // code readability should be high
+
+console.log("Abrar")
+
+
+let name = "Abrar"
+let age = 18
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
